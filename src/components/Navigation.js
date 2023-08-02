47c6dcb66e04dfca4b7227436bdcfdfd46ebac76
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-02 12:47:13 +0300

    added divs and style

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Navigation.scss"
+
+function Navigation (){
+  return (
+    <div className='navigation'>
+      <div className='container'>
+        <img className='logo' src="" alt="instagram-logo" />
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
